8ff9b182ca6b6293d6fd223484296b72aba3d041
Aleksei Dmitriev <alexdmspb@gmail.com>
2025-05-28 11:49:04 +0100

    fix: wallet type

--- lib/web3/wallets.ts
+++ lib/web3/wallets.ts
@@ -1,5 +1,6 @@
 import type { WalletType, WalletInfo } from 'types/client/wallets';
 
-export const WALLETS_INFO: Record<Exclude<WalletType, 'none'>, WalletInfo> = {
-
-};
+export const WALLETS_INFO: Record<
+  Exclude<WalletType, 'none'>,
+  WalletInfo
+> = {} as unknown as Record<Exclude<WalletType, 'none'>, WalletInfo>;
